cda4d6f3358835e05bb0487e9f972ea2e5f54464
princess JJ the meanie <jingjie105@gmail.com>
2024-06-13 02:03:20 +0800

    UPDATE: readme file, minor adjustments

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # tlextractor
+
+
+<h1>Requirements</h1>
+- pipenv
+- Python 3^
+
+<h1>Running the script</h1>
+- <b>pip install pipenv</b> to install virtual environment
+- <b>pipenv install</b> install dependencies
+- <b>pipenv shell</b> to run in virtual environment
+- <b>python tlextractor.py</b> to run the script
--- tlextractor.py
+++ tlextractor.py
@@ -1,28 +1,45 @@
-from urllib.request import urlopen
-import re
+import os
+os.environ['PYPPETEER_CHROMIUM_REVISION'] = '1230501'
 
+
+from colorama import Fore
+
+
+# TASKS
+# - Change to playwright for better performance
 
 
 url = str()
 target = []
 val = str()
+
+# Example: https://www.tldraw.com/r/eRZwoL-G5ufBB3KTRaSbW?v=-659,-888,5720,4826&p=HGtpLC0ipiTvgK6awql7m
 #pattern = "^http(s|):\/\/[0-9A-z.]+.[0-9A-z/.]+$"
+#tlui-button__menu
+
+
 
 # Extract data from the url
-def ExtractData():
-    # page = urlopen(url)
-    pass
+def ExtractData(frame):
+    # if url is invalid, exit program
+    
+    try:
+        pass
+    except Exception as e:
+        print(Fore.RED + str(e))
+        exit()
 
 
 
 
 
-# 1. add url checker in the future
 while url == "":
     url = input("Tldraw project url: ")
 
+
+print(Fore.LIGHTCYAN_EX + "\nType 'ALL' to extract all frames. Otherwise, enter the frame(s) you want to extract.")
 while True:
-    val = input("\n\nType 'ALL' first to extract all frames.\nOtherwise, enter the frame(s) you want to extract. When finished type 'DONE'.\n::").lower()
+    val = input("\nWhen finished type 'DONE'.\n::").lower()
 
     # Check if the user wants to extract all frames
     if (len(target) == 0 and val == "all"):
@@ -44,11 +61,15 @@ if (target[0] == "all"):
     # Extract all data
     pass
 else:
+    if (len(target) == 0):
+        print(Fore.RED + "No frames selected. Exiting program.")
+        exit()
+
     # loop and extract each frame
+    for i in target:
+        ExtractData(i)
 
-    pass
-
-print("Target: \n", target)
+    
 
 
 
